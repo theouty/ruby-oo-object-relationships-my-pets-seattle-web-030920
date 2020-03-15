4f78620e7b23bc0ca245e5b1a0d2e7617ff0460f
Aman Michael <aman.david.michael@gmail.com>
2020-03-15 05:10:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
-  # code goes here
+  attr_accessor :owner, 
+  attr_reader :name, :mood
+  
+  @@all =[]
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = nervous
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,22 @@
 class Owner
-  # code goes here
+attr_reader :name, :species
+
+@@all = []
+  def initialize()
+  @species = human 
+  end
+  
+  
+    
+    
+  def cats 
+    Cats.all.select{|cat| cat.owner == self}
+  end
+  
+  def dogs 
+    Dogs.all.select{|dog| dog.owner == self}
+  end
+  
+  def buy_cat 
+    
 end
